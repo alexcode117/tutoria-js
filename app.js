52d96d7da3885b0a31fcf91d7780ec8bf5797715
alexcode117 <alexcode7vzla@gmail.com>
2024-06-03 19:00:24 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,140 @@
+// import { player2 } from './consts'
+
+//let nombre = 'David'
+// let apellido = 'tovar'
+// let isFake = true
+// let price = 23
+// console.log(typeof(nombre));
+// console.log(typeof(price));
+// console.log(typeof(isFake));
+
+/*-----------------------------------------*/
+
+// let arr = ['hola', 3, false];
+// console.log(arr[2]);
+
+/*-----------------------------------------*/
+
+// let player = {
+//     name: 'Lucas',
+//     mana: 450,
+//     isPowered: true,
+//     skills: ['swim', 'shoot', 'sight'],
+//     pets: {
+//         dogs: ['dante'],
+//         cats: ['moon', 'safiro']
+//     }
+// }
+
+// console.log(player['mana'])
+// console.log(player.isPowered)
+// console.log(player.pets.dogs)
+// console.log(player.pets.cats[1])
+
+// const { skills } = player2
+// console.log(skills)
+
+/*-----------------------------------------*/
+
+// var + var
+//let newCompleteName = nombre + apellido;
+//console.log(newCompleteName)
+
+//Search if icludes
+//console.log(nombre.includes('d'))
+
+//Delete space
+// let newName = nombre.split('');
+// console.log(newName)
+
+//let str = 'Hola que tal'
+// let newName = nombre.split(' ');
+// console.log(newName)
+
+//let mensaje = 'Soy un tipo feliz y afortunado';
+//console.log(mensaje.split(' ', 4)); // ["Soy", "un", "tipo", "feliz"] 
+
+//Cutting space
+// let newName2 = nombre.slice(0, 2);
+// console.log(newName2)
+
+/*---------------------------------------------*/
+//arrays
+//let item = 'algo'
+//const nombres = ['david', 'gloria', 'rafa'];
+// const nombres2 = ['dante', 'paquita', 'chucho'];
+// let newArr = nombres.concat(nombres2);
+
+//Removing last item
+//let newArr = nombres.pop()
+
+//asdding last item
+// let newArr =nombres.push(item)
+
+//removing first item
+//let newArr = nombres.shift()
+
+//adding first item
+// let newArr = nombres.unshift(item)
+
+// let newArr = nombres.slice(0, 2)
+
+//console.log(newArr)
+
+/*-----------------------------------------*/
+// let edad = 15;
+// let dinero = 200;
+// const dni = true;
+// const licencia = false;
+// const palanca = false;
+
+// if(edad > 18){
+//     console.log('Eres mayor de edad');
+// } else {
+//     console.log('Eres menor')
+// }
+
+// if(edad > 18 && dinero > 100){
+//     console.log('Puedes entrar');
+// } else {
+//     console.log('No tienes acceso');
+// }
+
+// if(edad > 18){
+//     if(dinero > 100){
+//         console.log('Puedes entrar');
+//     } else {
+//         console.log('No tienes acceso');
+//     }
+// }
+
+// if(edad > 18){
+//     if(dinero > 100){
+//         console.log('Puedes entrar');
+//     } else {
+//         console.log('No tienes acceso');
+//     }
+// } else if(palanca) {
+//     console.log('Templao pudiste pasar')
+// } else {
+//     console.log('Te jodiste')
+// }
+
+// if(dni || licencia){
+//     console.log('Puedes entrar');
+// } else {
+//     console.log('No tienes acceso');
+// }
+
+/*-----------------------------------------*/
+// const arr = [2, 4, 8, 16];
+// const newArr = [];
+// let multi = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     multi = arr[i] * 2
+//     console.log(multi)
+//     newArr.push(multi)
+// }
+
+// console.log(arr, newArr)
---
+++ consts.js
@@ -0,0 +1,13 @@
+export let player2 = {
+     name: 'Lucas',
+     mana: 450,
+     isPowered: true,
+     skills: ['swim', 'shoot', 'sight'],
+     pets: {
+         dogs: ['dante'],
+         cats: ['moon', 'safiro']
+     }
+ }
+
+//  export { player2 };
+//  export default player2;
